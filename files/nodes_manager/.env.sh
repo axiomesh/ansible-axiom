4eda2f1965b5c65438027197aa391fe39892a40c
xiaochenl <nhdxlxc@163.com>
2023-12-15 11:23:13 +0800

    modify some scripts adapted to new version

--- files/nodes_manager/.env.sh
+++ files/nodes_manager/.env.sh
@@ -4,3 +4,9 @@
 export AXIOM_LEDGER_PROJECT_PATH="/home/lxc/code/axiom-ledger"
 # The path of axiom-ledger binary on the target machine
 export AXIOM_LEDGER_BINARY_PATH="/home/lxc/go/bin/axiom-ledger"
+
+export START_JSONRPC_PORT=3880
+export START_WEBSOCKET_PORT=3990
+export START_P2P_PORT=3000
+export START_PPROF_PORT=33120
+export START_MONITOR_PORT=30010
